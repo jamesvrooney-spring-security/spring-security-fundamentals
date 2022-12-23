79f428f09803374636576d2796fb4b8409c9559f
jamesvrooney <jamesvrooney@hotmail.com>
2022-12-23 00:21:03 +0000

    Initial commit.

---
+++ src/main/java/com/jamesvrooney/SpringSecurityLaurSpilcaApplication.java
@@ -0,0 +1,13 @@
+package com.jamesvrooney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityLaurSpilcaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityLaurSpilcaApplication.class, args);
+	}
+
+}
