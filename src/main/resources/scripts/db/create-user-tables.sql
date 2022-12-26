87b9db8e48ab7b19ac1bb7c27a34e862491522a3
jamesvrooney <jamesvrooney@hotmail.com>
2022-12-26 07:33:23 +0000

    Update db script.

--- src/main/resources/scripts/db/create-user-tables.sql
+++ src/main/resources/scripts/db/create-user-tables.sql
@@ -3,12 +3,12 @@ CREATE TABLE IF NOT EXISTS users
     id integer NOT NULL PRIMARY KEY,
     username character varying NOT NULL,
     password character varying NOT NULL
-)
+);
 
 CREATE TABLE IF NOT EXISTS authorities (
     id bigint NOT NULL PRIMARY KEY,
     name character varying NOT NULL
-)
+);
 
 CREATE TABLE IF NOT EXISTS users_authorities
 (
@@ -18,4 +18,4 @@ CREATE TABLE IF NOT EXISTS users_authorities
         REFERENCES authorities (id),
     CONSTRAINT "userIdFK" FOREIGN KEY (user_id)
         REFERENCES users (id)
-)
+);
